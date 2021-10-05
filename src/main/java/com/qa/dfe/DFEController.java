789dcbe97ed14af3d322a483c7909a54cf458c49
jordan <jharrison1@qa.com>
2021-10-05 09:55:43 +0100

    added response entities and swagger docs

--- src/main/java/com/qa/dfe/DFEController.java
+++ src/main/java/com/qa/dfe/DFEController.java
@@ -3,6 +3,8 @@ package com.qa.dfe;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,33 +32,35 @@ public class DFEController {
 		return "So long!";
 	}
 
-	@GetMapping("/getMarsupial/{id}")
+	@GetMapping("/getMarsupial/{id}") // 200
 	public Marsupial getMarsupialByIndex(@PathVariable Integer id) {
 
 		return this.marsupials.get(id);
 	}
 
-	@GetMapping("/getAllMarsupials")
+	@GetMapping("/getAllMarsupials") // 200
 	public List<Marsupial> getAllMarsupials() {
 
 		return this.marsupials;
 	}
 
-	@PostMapping("/createMarsupial")
-	public Marsupial createMarsupial(@RequestBody Marsupial marsupial) {
+	@PostMapping("/createMarsupial") // 201
+	public ResponseEntity<Marsupial> createMarsupial(@RequestBody Marsupial marsupial) {
 		System.out.println("CREATED MARSUPIAL: " + marsupial);
 		this.marsupials.add(marsupial);
-		return this.marsupials.get(this.marsupials.size() - 1);
+		Marsupial responseBody = this.marsupials.get(this.marsupials.size() - 1);
+		ResponseEntity<Marsupial> response = new ResponseEntity<Marsupial>(responseBody, HttpStatus.CREATED);
+		return response;
 	}
 
-	@PutMapping("/updateMarsupial/{id}")
+	@PutMapping("/updateMarsupial/{id}") // 202 - Accepted
 	public Marsupial updateMarsupial(@RequestBody Marsupial marsupial, @PathVariable Integer id) {
 		System.out.println("UPDATED MARSUPIAL: " + marsupial);
 		System.out.println("ID: " + id);
 		return this.marsupials.set(id, marsupial); // replaces the marsupial at that index
 	}
 
-	@DeleteMapping("/removeMarsupial/{id}")
+	@DeleteMapping("/removeMarsupial/{id}") // 204 - No content
 	public String deleteMarsupial(@PathVariable Integer id) {
 		Marsupial toDelete = this.marsupials.get(id);
 		this.marsupials.remove(toDelete);
